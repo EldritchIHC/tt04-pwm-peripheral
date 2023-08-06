ba37488e54fdc3b304e3e4719145bfd842fea77c
Medinceanu Paul-Catalin <ihatecreepers55@yahoo.com>
2023-08-06 09:41:42 +0300

    Update project_pwm_peripheral.v

--- src/project_pwm_peripheral.v
+++ src/project_pwm_peripheral.v
@@ -213,7 +213,7 @@ module tt_um_eldritch_pwm_peripheral(
         .i_action_period(w_pwm1A_action_register[3:2]),
         .i_action_compare_a(w_pwm1A_action_register[5:4]),
         .i_action_compare_b(w_pwm1A_action_register[7:6]),
-        .o_pwm(w_pwm1A),
+        .o_pwm(w_pwm1A)
         //.db_pwm()
     );
     
@@ -229,7 +229,7 @@ module tt_um_eldritch_pwm_peripheral(
         .i_action_period(w_pwm1B_action_register[3:2]),
         .i_action_compare_a(w_pwm1B_action_register[5:4]),
         .i_action_compare_b(w_pwm1B_action_register[7:6]),
-        .o_pwm(w_pwm1B),
+        .o_pwm(w_pwm1B)
         //.db_pwm()
     );
     
@@ -245,8 +245,8 @@ module tt_um_eldritch_pwm_peripheral(
         .i_action_period(w_pwm2A_action_register[3:2]),
         .i_action_compare_a(w_pwm2A_action_register[5:4]),
         .i_action_compare_b(w_pwm2A_action_register[7:6]),
-        .o_pwm(w_pwm2A),
-        .db_pwm()
+        .o_pwm(w_pwm2A)
+        //.db_pwm()
     );
     
     project_pwm_peripheral_comparator comparator2B(
@@ -261,7 +261,7 @@ module tt_um_eldritch_pwm_peripheral(
         .i_action_period(w_pwm2B_action_register[3:2]),
         .i_action_compare_a(w_pwm2B_action_register[5:4]),
         .i_action_compare_b(w_pwm2B_action_register[7:6]),
-        .o_pwm(w_pwm2B),
+        .o_pwm(w_pwm2B)
         //.db_pwm()
     );
     
@@ -277,7 +277,7 @@ module tt_um_eldritch_pwm_peripheral(
         .i_action_period(w_pwm3A_action_register[3:2]),
         .i_action_compare_a(w_pwm3A_action_register[5:4]),
         .i_action_compare_b(w_pwm3A_action_register[7:6]),
-        .o_pwm(w_pwm3A),
+        .o_pwm(w_pwm3A)
         //.db_pwm()
     );
     
@@ -293,7 +293,7 @@ module tt_um_eldritch_pwm_peripheral(
         .i_action_period(w_pwm3B_action_register[3:2]),
         .i_action_compare_a(w_pwm3B_action_register[5:4]),
         .i_action_compare_b(w_pwm3B_action_register[7:6]),
-        .o_pwm(w_pwm3B),
+        .o_pwm(w_pwm3B)
         //.db_pwm()
     );
     //Deadband
